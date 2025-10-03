84cd90a58a996b424d7202a4abf1128ad03886bf
Dawid Sac <dawidsac24@gmail.com>
2025-10-03 11:46:44 +0200

    adding the base project

--- App/Source/AppLayer.h
+++ App/Source/AppLayer.h
@@ -4,16 +4,16 @@
 
 #include "Core/Layer.h"
 
-class AppLayer : public Core::Layer
-{
+class AppLayer : public Core::Layer {
 public:
-	AppLayer();
-	virtual ~AppLayer();
+  AppLayer();
+  virtual ~AppLayer();
 
-	virtual void OnUpdate(float ts) override;
-	virtual void OnRender() override;
+  virtual void OnUpdate(float ts) override;
+  virtual void OnRender() override;
+
 private:
-	uint32_t m_Shader = 0;
-	uint32_t m_VertexArray = 0;
-	uint32_t m_VertexBuffer = 0;
+  uint32_t m_Shader = 0;
+  uint32_t m_VertexArray = 0;
+  uint32_t m_VertexBuffer = 0;
 };
--- App/Source/Main.cpp
+++ App/Source/Main.cpp
@@ -2,14 +2,14 @@
 
 #include "AppLayer.h"
 
-int main()
-{
-	Core::ApplicationSpecification appSpec;
-	appSpec.Name = "Architecture";
-	appSpec.WindowSpec.Width = 1920;
-	appSpec.WindowSpec.Height = 1080;
+int main() {
+  Core::ApplicationSpecification appSpec;
+  appSpec.Name = "Architecture";
+  appSpec.WindowSpec.Width = 1920;
+  appSpec.WindowSpec.Height = 1080;
 
-	Core::Application application(appSpec);
-	application.PushLayer<AppLayer>();
-	application.Run();
+  Core::Application application(appSpec);
+  application.PushLayer<AppLayer>();
+  application.Run();
+  
 }
